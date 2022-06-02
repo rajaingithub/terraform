cc6a02f0095595d78d69d0dc867fde1ea0e0e226
Pavan <raoraja994@gmail.com>
2022-06-02 17:24:41 +0100

    first terraform script for creating ec2-instance

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">=0.14.9"
+}
+provider "aws" {
+  profile = "Pavancreds"
+  region  = "us-east-1"
+}
+resource "aws_instance" "example_instance" {
+  ami           = "ami-0022f774911c1d690"
+  instance_type = "t2.micro"
+
+  tags = {
+    "Name" = "example instance"
+  }
+}
